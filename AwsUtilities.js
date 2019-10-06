558b37815961734d97bdae15d6077f8234c56ab9
jciangarotti <ciangarotti@gmail.com>
2019-10-06 11:29:35 -0300

    publishToSns now is a promise (wrapped with a promise function).

--- AwsUtilities.js
+++ AwsUtilities.js
@@ -115,20 +115,24 @@ class AwsUtilities {
            Message: JSON.stringify(message),
            TopicArn
         };
-
+        return new Promise((resolve, reject) => {
         // Create promise and SNS service object
         var publishTextPromise = new AWS.SNS({
-            apiVersion: '2010-03-31'
-        }).publish(params).promise();
+          apiVersion: '2010-03-31'
+        })
+        .publish(params).promise();
         // handle promise's fulfilled/rejected states
         publishTextPromise
-            .then((data) => {
-            // console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`);
-            // console.log(params.Message);
-            console.log("MessageID is " + data.MessageId);
+          .then((data) => {
+          // console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`);
+          // console.log(params.Message);
+          console.log("MessageID is " + data.MessageId);
+          resolve(data);
         }).catch((err) => {
-            console.error(err, err.stack);
-        });         
+          console.log(err);
+          reject(err);
+        }); 
+      });        
     };
 
     /**
